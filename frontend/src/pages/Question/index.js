e3c00eede3f3496d5c88699a05d877e4fcf091fc
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-04-22 00:41:21 -0300

    :lipstick: Começando interface de criação de perguntas quiz.

--- frontend/src/pages/Question/index.js
+++ frontend/src/pages/Question/index.js
@@ -1,8 +1,41 @@
 import React from 'react'
 
+import { Grid } from '@material-ui/core'
+
+
+import {
+  StyledGrid,
+  ContainerGrid,
+  StyledAddQuestionButton,
+  StyledFieldOfQuestion,
+  StyledActionsButton
+} from './style'
+
 const Question = () => {
   return (
-    <p>teste</p>
+    <ContainerGrid container justify='space-between'>
+      <Grid item xs={2}>
+        <StyledGrid container>questoes</StyledGrid>
+      </Grid>
+      <Grid item xs={8}>
+        <Grid container>
+          <StyledAddQuestionButton container justify='space-around'>
+            <Grid item xs={3} style={{'background': 'red'}}><p>Botao 1</p></Grid>
+            <Grid item xs={3} style={{'background': 'red'}}><p>Botao 2</p></Grid>
+          </StyledAddQuestionButton>
+          <StyledFieldOfQuestion container>
+            questão
+          </StyledFieldOfQuestion>
+          <StyledActionsButton container justify='space-around'>
+            <Grid item xs={3} style={{'background': 'red'}}><p>Cancelar</p></Grid>
+            <Grid item xs={3} style={{'background': 'red'}}><p>Finalizar</p></Grid>
+          </StyledActionsButton>
+        </Grid>
+      </Grid>
+      <Grid item xs={2}>
+        <StyledGrid container>detalhes</StyledGrid>
+      </Grid>
+    </ContainerGrid>
   )
 }
 
---
+++ frontend/src/pages/Question/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+import { Grid } from '@material-ui/core'
+
+export const ContainerGrid = styled(Grid)`
+  
+`
+
+export const StyledGrid = styled(Grid)`
+  /* width: 20%; */
+  height: 100vh;
+  background: palevioletred;
+`
+
+export const StyledAddQuestionButton = styled(Grid)`
+  background: blue;
+`
+
+export const StyledFieldOfQuestion = styled(Grid)`
+  background: green;
+  height: calc(100vh - 48px - 48px);
+`
+
+export const StyledActionsButton = styled(Grid)`
+  background: blue;
+`
