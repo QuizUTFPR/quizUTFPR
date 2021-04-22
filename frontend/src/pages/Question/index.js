4fe8453fd47fb4ea7b7667bcd5262858041dfcf0
Jesse Pires <jessepires2010@gmail.com>
2021-04-22 13:12:03 -0300

    :lipstick: Início da criação do layout da página de criação de questões

--- frontend/src/pages/Question/index.js
+++ frontend/src/pages/Question/index.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
 
-import { Grid } from '@material-ui/core'
-
+import { Grid, Typography, Button, Divider } from "@material-ui/core";
 
 import {
   StyledGrid,
@@ -9,34 +8,59 @@ import {
   StyledAddQuestionButton,
   StyledFieldOfQuestion,
   StyledActionsButton
-} from './style'
+} from "./style";
 
 const Question = () => {
   return (
-    <ContainerGrid container justify='space-between'>
+    <ContainerGrid container justify="space-between">
       <Grid item xs={2}>
-        <StyledGrid container>questoes</StyledGrid>
+        <StyledGrid container align="center" justify="center">
+          <Typography color="primary" component="h5" variant="h5">
+            Questões
+          </Typography>
+        </StyledGrid>
       </Grid>
+
       <Grid item xs={8}>
         <Grid container>
-          <StyledAddQuestionButton container justify='space-around'>
-            <Grid item xs={3} style={{'background': 'red'}}><p>Botao 1</p></Grid>
-            <Grid item xs={3} style={{'background': 'red'}}><p>Botao 2</p></Grid>
-          </StyledAddQuestionButton>
-          <StyledFieldOfQuestion container>
-            questão
+          <Grid container justify="space-around">
+            <Grid item xs={3} align="center" justify="center">
+              <Button fullwidth variant="contained" color="primary">
+                CRIAR NOVA QUESTÃO
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullwidth variant="contained" color="primary">
+                USAR QUESTÃO DO BANCO
+              </Button>
+            </Grid>
+          </Grid>
+          <StyledFieldOfQuestion container justify="center">
+            <Typography>Título da Questão...</Typography>
           </StyledFieldOfQuestion>
-          <StyledActionsButton container justify='space-around'>
-            <Grid item xs={3} style={{'background': 'red'}}><p>Cancelar</p></Grid>
-            <Grid item xs={3} style={{'background': 'red'}}><p>Finalizar</p></Grid>
-          </StyledActionsButton>
+          <Grid container justify="space-around">
+            <Grid item xs={3} align="center" justify="center">
+              <Button fullwidth variant="contained" color="secondary">
+                CANCELAR
+              </Button>
+            </Grid>
+            <Grid item xs={3} align="center" justify="center">
+              <Button fullwidth variant="contained" color="primary">
+                FINALIZAR
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
       <Grid item xs={2}>
-        <StyledGrid container>detalhes</StyledGrid>
+        <StyledGrid container align="center" justify="center">
+          <Typography color="primary" component="h5" variant="h5">
+            Detalhes
+          </Typography>
+        </StyledGrid>
       </Grid>
     </ContainerGrid>
-  )
-}
+  );
+};
 
 export default Question;
--- frontend/src/pages/Question/style.js
+++ frontend/src/pages/Question/style.js
@@ -1,26 +1,24 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import { Grid } from '@material-ui/core'
+import { Grid } from "@material-ui/core";
 
-export const ContainerGrid = styled(Grid)`
-  
-`
+export const ContainerGrid = styled(Grid)``;
 
 export const StyledGrid = styled(Grid)`
   /* width: 20%; */
   height: 100vh;
-  background: palevioletred;
-`
+  /* background: palevioletred; */
+`;
 
 export const StyledAddQuestionButton = styled(Grid)`
-  background: blue;
-`
+  /* background: blue; */
+`;
 
 export const StyledFieldOfQuestion = styled(Grid)`
-  background: green;
+  /* background: green; */
   height: calc(100vh - 48px - 48px);
-`
+`;
 
 export const StyledActionsButton = styled(Grid)`
   background: blue;
-`
+`;
