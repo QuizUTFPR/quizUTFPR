ecdf074dd89eb2ffe332bf6c17b6b9ed69baee56
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-04-24 19:28:30 -0300

    :lipstick: Estilizando modal de buscar questões no banco via tag.

--- frontend/src/pages/Question/QuestionDatabase/question.js
+++ frontend/src/pages/Question/QuestionDatabase/question.js
@@ -1,33 +1,62 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import {
   Card,
   CardContent,
   Typography,
-  CardActions ,
-  Button,
+  // CardActions ,
+  // Button,
   Divider,
   Grid
 } from '@material-ui/core';
 
+const defaultMargin = '10px';
+
+const Answer = styled(Typography)`
+  padding: 10px;
+  border-radius: 5px;
+`
+
+const WrongAnswer = styled(Answer)`
+  background: red;
+`
+
+const CorrectgAnswer = styled(Answer)`
+  background: green;
+`
+
+const TitleQuestion = styled(Typography)`
+  margin-top: ${defaultMargin};
+  margin-bottom: ${defaultMargin};
+  font-weight: bolder;
+`
 
 const Questions = () => {
   return (
     <Card  variant="outlined">
       <CardContent>
-        <Grid container>
-          <Typography color="textSecondary" gutterBottom>Multípla Escolha</Typography>
+        <Grid container justify='space-between'>
+          <Grid item>
+            <Typography color="textSecondary" gutterBottom>Multípla Escolha</Typography>
+          </Grid>
+          <Grid item>
+            <Typography color="textSecondary" gutterBottom>Fácil</Typography>
+          </Grid>
         </Grid>
         <Divider />
-        <Typography variant="h5" component="h2" color='primary'>
+        <TitleQuestion variant="h5" component="h2" color='primary'>
           Título da Questão
-        </Typography>
+        </TitleQuestion>
         <Typography  color="textSecondary">
           Alternativas
         </Typography>
-        <Typography variant="body2" component="p">
-          colocar grid aqui com questões
-        </Typography>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}><WrongAnswer>Alternativa Errada</WrongAnswer></Grid>
+          <Grid item xs={12} md={6}><CorrectgAnswer>Alternativa Correta</CorrectgAnswer></Grid>
+          <Grid item xs={12} md={6}><WrongAnswer>Alternativa Errada</WrongAnswer></Grid>
+          <Grid item xs={12} md={6}><WrongAnswer>Alternativa Errada</WrongAnswer></Grid>
+        </Grid>
       </CardContent>
       {/* <CardActions>
         <Button size="small">Learn More</Button>
