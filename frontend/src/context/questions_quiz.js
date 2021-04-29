17ebf8bb0e70685613d576d2e35f1480fd4b8ad8
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-04-29 08:35:21 -0300

    :sparkles: Informando formato de requisição para criar questões.

--- frontend/src/context/questions_quiz.js
+++ frontend/src/context/questions_quiz.js
@@ -4,7 +4,24 @@ import PropTypes from 'prop-types';
 export const QuestionQuizContext = createContext();
 
 const QuestionQuiz = ({ children }) => {
-  const [questions, setQuestions] = useState('funcionando!');
+  const [questions, setQuestions] = useState([{
+    title: "Chabu é erro?",
+    correctAnswer: true,
+    timer: 15,
+    difficultyLevel: 3,
+    quiz_id: 2,
+    answer: [
+        {
+            title: "Verdadeiro",
+            is_correct: true
+        },
+        {
+            title: "Mentira",
+            is_correct: false
+        }
+    ],
+    tags: [{name: "erro"}]
+  }]);
 
 
   return (
