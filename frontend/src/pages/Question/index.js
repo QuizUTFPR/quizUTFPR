f73487668241489f0f956af4abac47b516079276
Jesse Pires <jessepires2010@gmail.com>
2021-05-06 12:45:07 -0300

    :lipstick: Criando layout dos detalhes da questão

--- frontend/src/pages/Question/index.js
+++ frontend/src/pages/Question/index.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { useFormik } from "formik";
 
+import ChipInput from "@components/ChipInput";
+
 import {
   Grid,
   Typography,
   TextField,
   Toolbar,
-  Button
+  Button,
+  MenuItem
 } from "@material-ui/core";
 
 import {
@@ -26,42 +29,42 @@ import useQuestionQuiz from "@hooks/QuestionQuiz";
 
 // COMPONENTS
 import Modal from "@components/Modal";
-import StyledButton from '@components/Button'
+import StyledButton from "@components/Button";
 
 // PAGES
-import TypeOfQuestion from '../TypeOfQuestion'
-
+import TypeOfQuestion from "../TypeOfQuestion";
 
 const Question = () => {
   const { questions } = useQuestionQuiz();
 
-  const [isModalTypeOfQuestionOpen, setModalTypeOfQuestionOpen] = useState(false);
+  const [isModalTypeOfQuestionOpen, setModalTypeOfQuestionOpen] = useState(
+    false
+  );
   const handleOpenModalTypeQuestion = () => setModalTypeOfQuestionOpen(true);
   const handleCloseModalTypeQuestion = () => setModalTypeOfQuestionOpen(false);
-  
+
   const [questionOnScreen, setQuestionOnScreen] = useState(questions[0]);
 
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: questionOnScreen,
     onSubmit: values => {
-      console.log(values)
+      console.log(values);
     }
   });
 
-  const handleChangeQuestion = (question) => () => {
+  const handleChangeQuestion = question => () => {
     setQuestionOnScreen(question);
-  }
+  };
 
   return (
     <>
       <StyledAppBar position="static" color="transparent">
         <Toolbar>
-          <Grid container justify="space-between" alignItems='center'>
+          <Grid container justify="space-between" alignItems="center">
             <StyledButton color="secondary" variant="outlined">
               SAIR
             </StyledButton>
-            
 
             <Typography component="h4" variant="h4" color="primary">
               Título do Quiz
@@ -74,9 +77,7 @@ const Question = () => {
         </Toolbar>
       </StyledAppBar>
 
-
       <ContainerGrid container>
-
         {/* LEFT */}
         <Grid item xs={2}>
           <StyledLeftGrid container align="center">
@@ -89,16 +90,22 @@ const Question = () => {
             <Grid container>
               {questions.map((item, index) => (
                 <Grid item xs={12} key={index}>
-                  <Button color="primary" variant="outlined" fullWidth onClick={handleChangeQuestion(item)}>
+                  <Button
+                    color="primary"
+                    variant="outlined"
+                    fullWidth
+                    onClick={handleChangeQuestion(item)}
+                  >
                     {item.title}
                   </Button>
                 </Grid>
               ))}
             </Grid>
             <GridButtonNewQuestion item xs={12}>
-              <StyledButton 
-                onClick={handleOpenModalTypeQuestion} 
-                fullWidth variant="contained" 
+              <StyledButton
+                onClick={handleOpenModalTypeQuestion}
+                fullWidth
+                variant="contained"
                 color="secondary"
               >
                 CRIAR NOVA QUESTÃO
@@ -107,69 +114,126 @@ const Question = () => {
           </StyledLeftGrid>
         </Grid>
 
-
         {/* MIDDLE */}
         <Grid item xs={8}>
-            <StyledGrid container justify="center" align='center' component='form' onSubmit={formik.handleSubmit}>
-              <Grid item xs={12}>
-                <StyledTitleInput
-                  fullWidth
-                  placeholder="DIGITE O ENUNCIADO AQUI"
-                  id="title"
-                  required
-                  autoFocus
-                  value={formik.values.title}
-                  onChange={formik.handleChange}
-                />
-              </Grid>
+          <StyledGrid
+            container
+            justify="center"
+            align="center"
+            component="form"
+            onSubmit={formik.handleSubmit}
+          >
+            <Grid item xs={12}>
+              <StyledTitleInput
+                fullWidth
+                placeholder="DIGITE O ENUNCIADO AQUI"
+                id="title"
+                required
+                autoFocus
+                value={formik.values.title}
+                onChange={formik.handleChange}
+              />
+            </Grid>
 
-              <Grid item>
-                <TextField
-                  type="file"
-                  name="Imagem de Capa"
-                  id="file"
-                  onChange={event =>
-                    console.log("file:", event.target.files[0])
-                  }
-                />
-              </Grid>
+            <Grid item>
+              <TextField
+                type="file"
+                name="Imagem de Capa"
+                id="file"
+                onChange={event => console.log("file:", event.target.files[0])}
+              />
+            </Grid>
 
-              <Grid container align='center' justify='center' spacing={2}>
-                {formik.values.answer.map((item, index) => (
-                  <Grid item xs={12} md={6} key={index}>
-                    <StyledAnswerInput
-                      type="text"
-                      placeholder={`DIGITE A ALTERNATIVA ${index+1}`}
-                      id={`answer[${index}].title`}
-                      value={item.title}
-                      onChange={formik.handleChange}
-                      required
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-            
+            <Grid container align="center" justify="center" spacing={2}>
+              {formik.values.answer.map((item, index) => (
+                <Grid item xs={12} md={6} key={index}>
+                  <StyledAnswerInput
+                    type="text"
+                    placeholder={`DIGITE A ALTERNATIVA ${index + 1}`}
+                    id={`answer[${index}].title`}
+                    value={item.title}
+                    onChange={formik.handleChange}
+                    required
+                  />
+                </Grid>
+              ))}
+            </Grid>
+
             <GridRegisterQuestion item xs={12}>
-              <StyledButton type='submit' fullWidth color="primary" variant="contained">
-                  SALVAR ALTERAÇÕES
+              <StyledButton
+                type="submit"
+                fullWidth
+                color="primary"
+                variant="contained"
+              >
+                SALVAR ALTERAÇÕES
               </StyledButton>
             </GridRegisterQuestion>
-            </StyledGrid>
-          </Grid>
-          
+          </StyledGrid>
+        </Grid>
 
         {/* RIGHT */}
         <Grid item xs={2}>
-          <StyledRightGrid container align="center" justify="center">
+          <StyledRightGrid
+            container
+            spacing={3}
+            align="center"
+            direction="column"
+          >
             <Grid item>
               <Typography color="primary" component="h5" variant="h5">
                 Detalhes
               </Typography>
             </Grid>
+
+            <Grid item>
+              <TextField
+                fullWidth
+                label="Tempo"
+                id="time"
+                name="time"
+                variant="outlined"
+                InputLabelProps={{ shrink: true }}
+                // value={formik.values.visibility}
+                // onChange={event =>
+                //   formik.setFieldValue("visibility", event.target.value)
+                // }
+                required
+                select
+              >
+                <MenuItem value={30}>30 segundos</MenuItem>
+              </TextField>
+            </Grid>
+
+            <Grid item>
+              <TextField
+                fullWidth
+                label="Alternativa Correta"
+                id="correctAnswer"
+                name="correctAnswer"
+                variant="outlined"
+                InputLabelProps={{ shrink: true }}
+                // value={formik.values.visibility}
+                // onChange={event =>
+                //   formik.setFieldValue("visibility", event.target.value)
+                // }
+                required
+                select
+              >
+                <MenuItem value="1">1</MenuItem>
+              </TextField>
+            </Grid>
+            <Grid item>
+              <ChipInput
+                fullWidth
+                suggestions={["Aprenda", "JavaScript"]}
+                value={formik.values.tags}
+                id="tags"
+                onChange={formik.handleChange}
+              />
+            </Grid>
           </StyledRightGrid>
         </Grid>
-
-
       </ContainerGrid>
 
       {/* MODALS */}
@@ -179,10 +243,7 @@ const Question = () => {
         modalDescription="Escolha o tipo da questão..."
         style={{ overflow: "scroll" }}
       >
-        <TypeOfQuestion
-          handleClose={handleCloseModalTypeQuestion}
-        />
-        
+        <TypeOfQuestion handleClose={handleCloseModalTypeQuestion} />
       </Modal>
     </>
   );
