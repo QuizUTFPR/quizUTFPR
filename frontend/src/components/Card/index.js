9089e23772b90eb89db640ad5d51a7bd07f47243
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-05-06 11:08:45 -0300

    :lipstick: Adicionando link no card dos quizzes.

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -6,20 +6,23 @@ import {
   StyledCardActions
 } from "./style";
 
-import { Typography } from "@material-ui/core";
-
+import { Typography, CardActionArea} from "@material-ui/core";
+import { Link } from 'react-router-dom'
+import { QUESTION } from '@routes';
 const Card = ({ image, imageTitle, title, description, children }) => {
   return (
     <StyledCard>
-      <StyledCardMedia image={image} title={imageTitle} />
-      <StyledCardContent>
-        <Typography color="primary" component="h5" variant="h5">
-          {title}
-        </Typography>
-        <Typography variant="subtitle1" color="textSecondary">
-          {description}
-        </Typography>
-      </StyledCardContent>
+      <StyledCardMedia component='img' image={image} title={imageTitle} />
+        <CardActionArea component={Link} to={QUESTION}>
+        <StyledCardContent>
+          <Typography color="primary" component="h5" variant="h5">
+            {title}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {description}
+          </Typography>
+        </StyledCardContent>
+        </CardActionArea>
       <StyledCardActions>{children}</StyledCardActions>
     </StyledCard>
   );
