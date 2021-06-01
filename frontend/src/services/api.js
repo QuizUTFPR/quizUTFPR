e5272ba77c93aa525d6f6575aaba38c4544a6623
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-01 16:12:11 -0300

    :sparkles: Deslogando professor quando token expirar.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import { TOKENEXPIRED } from '@routes';
+
 const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
@@ -12,18 +14,20 @@ api.interceptors.request.use((config) => {
 
 export default api;
 
-axios.interceptors.response.use(
-  (response) => {
-    if (response.status === 401) {
-      alert('You are not authorized');
-    }
-    console.log(response);
-    return response;
-  },
+api.interceptors.response.use(
+  (response) =>
+    // Do something with response data
+    response,
   (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(error.response.data);
+    // Do something with response error
+
+    // You can even test for a response code
+    // and try a new request before rejecting the promise
+    if (error.response.status === 401) {
+      localStorage.clear('@TOKEN');
+      localStorage.clear('@TEACHER');
+      window.location = TOKENEXPIRED;
     }
-    return Promise.reject(error.message);
+    return Promise.reject(error);
   }
 );
