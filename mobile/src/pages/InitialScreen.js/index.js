081f8625f47e9416f2557952f6c248251e7b4772
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-02 16:54:58 -0300

    :sparkles: teste.!

--- mobile/src/pages/InitialScreen.js/index.js
+++ mobile/src/pages/InitialScreen.js/index.js
@@ -4,17 +4,15 @@ import { Image, ImageBackground } from 'react-native';
 // STYLES
 import { Container } from './styles';
 
-const InitialScreen = () => {
-  return (
-    <Container>
-      {/* <ImageBackground
+const InitialScreen = () => (
+  <Container>
+    {/* <ImageBackground
         style={{ width: '100%', height: '100%' }}
         source={require('assets/waves.svg')}
       >
         <Image source={require('assets/login_illustration.svg')} />
       </ImageBackground> */}
-    </Container>
-  );
-};
+  </Container>
+);
 
 export default InitialScreen;
