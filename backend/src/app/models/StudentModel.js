e8075856fe0b52d7c894de49c5895504428c090d
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-04 17:16:26 -0300

    :card_file_box: Criando migration de student.

---
+++ backend/src/app/models/StudentModel.js
@@ -0,0 +1,41 @@
+import Sequelize, { Model } from "sequelize";
+import bcrypt from "bcryptjs";
+
+class Student extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        id_image: Sequelize.INTEGER
+      },
+      {
+        sequelize,
+        tableName: "student"
+      }
+    );
+
+    this.addHook("beforeSave", async student => {
+      if (student.password) {
+        student.password_hash = await bcrypt.hash(student.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, {
+      foreignKey: "id_image",
+      as: 'image_profile'
+    })
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default Student;
---
+++ backend/src/database/migrations/20210604200532-student.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('student', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'Sem Nome'
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      id_image: {
+        type: Sequelize.INTEGER,
+        foreignKey: true,
+        references: { model: "file", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('student');
+  }
+};
