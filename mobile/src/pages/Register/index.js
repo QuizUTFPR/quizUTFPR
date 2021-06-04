49318d385356cd6328abd79a50dd5ad1005e9d7a
Jesse Pires <jessepires2010@gmail.com>
2021-06-04 14:46:53 -0300

    :lipstick: Criando componente que permite fechar o teclado ao clicar em qualquer lugar fora do input

--- mobile/src/pages/Register/index.js
+++ mobile/src/pages/Register/index.js
@@ -5,6 +5,7 @@ import { useTheme } from 'react-native-paper';
 import Container from '@components/Container';
 import Input from '@components/Input';
 import Button from '@components/Button';
+import DismissKeyboard from '@components/DismissKeyboard';
 
 // STYLES
 import {
@@ -36,16 +37,23 @@ const Register = ({ navigation }) => {
         Por favor, informe seus dados
       </StyledText>
 
-      <InputWrapper>
-        <Input labelText="Nome de Usuário" mode="outlined" icon="account" />
-        <Input secureTextEntry labelText="Senha" mode="outlined" icon="lock" />
-        <Input
-          secureTextEntry
-          labelText="Confirmar Senha"
-          mode="outlined"
-          icon="lock"
-        />
-      </InputWrapper>
+      <DismissKeyboard>
+        <InputWrapper>
+          <Input labelText="Nome de Usuário" mode="outlined" icon="account" />
+          <Input
+            secureTextEntry
+            labelText="Senha"
+            mode="outlined"
+            icon="lock"
+          />
+          <Input
+            secureTextEntry
+            labelText="Confirmar Senha"
+            mode="outlined"
+            icon="lock"
+          />
+        </InputWrapper>
+      </DismissKeyboard>
 
       <WrapperButton>
         <Button onPress={() => console.log('pressed')} icon="login-variant">
