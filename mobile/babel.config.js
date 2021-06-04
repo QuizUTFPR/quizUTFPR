097e5af683b87469fc064b6432d3e63b938e1652
Jesse Pires <jessepires2010@gmail.com>
2021-06-03 23:50:06 -0300

    :lipstick: Criação da tela de cadastro

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -10,6 +10,7 @@ module.exports = function (api) {
             '@pages': './src/pages/',
             '@assets': './assets/',
             '@components': './src/components/',
+            '@styles': './src/styles',
           },
         },
       ],
