8f69bb2393041ab51bf2723ec763dd4b592e9566
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-07 19:49:54 -0300

    :sparkles: Criando controller que cria student.

---
+++ backend/src/app/controllers/Student/StudentController.js
@@ -0,0 +1,45 @@
+import * as Yup from "yup";
+import authConfig from '../../../config/auth'
+import jwt from 'jsonwebtoken'
+
+// MODELS
+import Student from "../../models/StudentModel";
+
+class StudentController {
+  // Cadastra um único registro
+  async store(req, res) {
+    try{
+      const schema = Yup.object().shape({
+        email: Yup.string().email().required(),
+        password: Yup.string().required()
+      });
+
+      if(!(await schema.isValid(req.body))){
+        return res.status(400).json({error: 'Falha na validação!'});
+      }
+
+      const {email, password} = req.body;
+
+      //cadastro professor caso nao existir no sistema
+      const student = await Student.create({ email: email, password: password });
+
+
+      const { id, name } = student;
+
+      return res.json({
+        student: {
+          name,
+          email,
+        },
+        token: jwt.sign({id}, authConfig.secret, {
+          expiresIn: authConfig.expireIn
+        })
+      });
+
+    }catch(err){
+      return res.status(500).json(err)
+    }
+    }
+}
+
+export default new StudentController();
