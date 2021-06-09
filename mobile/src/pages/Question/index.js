e22ab2d3400775e3330cbbed5520840b5518d944
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-09 20:45:24 -0300

    :bug: Testando lottie.

--- mobile/src/pages/Question/index.js
+++ mobile/src/pages/Question/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IconButton, useTheme } from 'react-native-paper';
-
+import LottieView from 'lottie-react-native';
+import { View } from 'react-native';
 // STYLES
 import {
   QuestionContainer,
@@ -21,44 +22,71 @@ import {
 const fakeAnswers = ['Amarelo', 'Roxo', 'Azul', 'Tijolo'];
 
 const Question = () => {
+  const animation = React.useRef(false);
   const { label } = useTheme();
 
+  React.useEffect(() => {
+    if (animation) {
+      animation.current.play();
+    }
+  }, []);
+
   return (
-    <QuestionContainer>
-      <QuestionWrapper>
-        <Header>
-          <IconButton icon="close" onPress={() => {}} />
-          <CurrentQuestion fontSize={label.fontSize}>
-            Questão 01/20
-          </CurrentQuestion>
-          <IconButton icon="timer" onPress={() => {}} />
-        </Header>
+    <>
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <LottieView
+          autoPlay
+          loop
+          ref={animation}
+          style={{
+            width: 500,
+            height: 500,
+          }}
+          // eslint-disable-next-line global-require
+          source={require('@assets/like.json')}
+        />
+      </View>
 
-        <InformationsWrapper>
-          <ScrollWrapper>
-            <QuestionDescription>
-              {/* eslint-disable-next-line global-require */}
-              <QuestionImage source={require('@assets/icon.png')} />
-              <QuestionText fontSize={label.fontSize}>
-                Qual a cor do líquido de Erlenmeyer?
-              </QuestionText>
-            </QuestionDescription>
+      <QuestionContainer>
+        <QuestionWrapper>
+          <Header>
+            <IconButton icon="close" onPress={() => {}} />
+            <CurrentQuestion fontSize={label.fontSize}>
+              Questão 01/20
+            </CurrentQuestion>
+            <IconButton icon="timer" onPress={() => {}} />
+          </Header>
 
-            {fakeAnswers.map((answer) => (
-              <AnswerContainer key={answer}>
-                <AnswerText fontSize={label.fontSize}>{answer}</AnswerText>
-              </AnswerContainer>
-            ))}
-          </ScrollWrapper>
-          <Footer>
-            <StyledButton mode="text" onPress={() => {}}>
-              Pular
-            </StyledButton>
-            <StyledButton onPress={() => {}}>Confirmar</StyledButton>
-          </Footer>
-        </InformationsWrapper>
-      </QuestionWrapper>
-    </QuestionContainer>
+          <InformationsWrapper>
+            <ScrollWrapper>
+              <QuestionDescription>
+                {/* eslint-disable-next-line global-require */}
+                <QuestionImage source={require('@assets/icon.png')} />
+                <QuestionText fontSize={label.fontSize}>
+                  Qual a cor do líquido de Erlenmeyer?
+                </QuestionText>
+              </QuestionDescription>
+
+              {fakeAnswers.map((answer) => (
+                <AnswerContainer key={answer}>
+                  <AnswerText fontSize={label.fontSize}>{answer}</AnswerText>
+                </AnswerContainer>
+              ))}
+            </ScrollWrapper>
+            <Footer>
+              <StyledButton mode="text" onPress={() => {}}>
+                Pular
+              </StyledButton>
+              <StyledButton onPress={() => {}}>Confirmar</StyledButton>
+            </Footer>
+          </InformationsWrapper>
+        </QuestionWrapper>
+      </QuestionContainer>
+    </>
   );
 };
 
