9e39a6e14e18a5658b44beded54c1c256cf8cff2
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-16 17:14:56 -0300

    :sparkles: Retirando padding do topo do container

--- mobile/src/components/Container/style.js
+++ mobile/src/components/Container/style.js
@@ -1,9 +1,4 @@
 import styled from 'styled-components/native';
-import Constants from 'expo-constants';
-import { Platform } from 'react-native';
-
-const statusBarHeight =
-  Platform.OS === 'android' ? Constants.statusBarHeight : 0;
 
 export const Container = styled.SafeAreaView`
   flex: 1;
@@ -13,7 +8,6 @@ export const Container = styled.SafeAreaView`
 export const Wrapper = styled.View`
   flex: 1;
   align-items: center;
-  padding-top: ${`${statusBarHeight}px`};
   padding-left: 30px;
   padding-right: 30px;
 `;
