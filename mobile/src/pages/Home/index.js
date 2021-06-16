f874dd29f5ab99bfbbaba49b7de70f76428e72ec
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-16 14:55:55 -0300

    :lipstick: Estilizando header da página home.

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 // COMPONENTS
 import Container from '@components/Container';
-import { Text } from 'react-native';
+import { Text, ImageBackground } from 'react-native';
 
-const Home = () => (
-  <Container>
-    <Text>Conteudis aqui!</Text>
-  </Container>
-);
+// ICONS
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
+
+// STYLES
+import {
+  HeaderWrapper,
+  HeaderInformations,
+  HeaderWelcomeTextView,
+  StyledTitle,
+  StyledParagraph,
+  HeaderButton,
+  SearchInput,
+  InputWrapper,
+  BackgroundHeader,
+} from './styles';
+
+const Home = () => {
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <HeaderWrapper>
+        <BackgroundHeader>
+          <HeaderInformations>
+            <HeaderButton onPress={() => navigation.openDrawer()}>
+              <AntDesign name="menu-fold" size={32} color="white" />
+            </HeaderButton>
+            <HeaderWelcomeTextView>
+              <StyledTitle fill="white">Seja bem-vindo,</StyledTitle>
+              <StyledParagraph fill="white">
+                Escolha um quiz e divirta-se!
+              </StyledParagraph>
+            </HeaderWelcomeTextView>
+          </HeaderInformations>
+
+          <InputWrapper>
+            <FontAwesome name="search" size={25} color="#4B24B1" />
+            <SearchInput placeholder="Digite o PIN" />
+          </InputWrapper>
+        </BackgroundHeader>
+      </HeaderWrapper>
+      <Text>Conteudis aqui!</Text>
+    </Container>
+  );
+};
 
 export default Home;
--- mobile/src/pages/Home/styles.js
+++ mobile/src/pages/Home/styles.js
@@ -1,7 +1,63 @@
 import styled from 'styled-components/native';
 
+// ASSETS
+import HeaderBackground from '@assets/patterns/sunburst.jpg';
+
+// DIMENSION TRANSFORMERS
+import { widthPercentageToDp, heightPercentageToDp } from '@styles/dimensions';
+
+export const HeaderWrapper = styled.SafeAreaView``;
+
+export const BackgroundHeader = styled.ImageBackground.attrs({
+  source: HeaderBackground,
+  resizeMode: 'cover',
+  imageStyle: {
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+  },
+})`
+  justify-content: center;
+  width: ${widthPercentageToDp('100%')}px;
+  height: ${heightPercentageToDp('30%')}px;
+`;
+
+export const HeaderInformations = styled.SafeAreaView`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+export const HeaderWelcomeTextView = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})``;
+
+export const InputWrapper = styled.View`
+  background: white;
+  flex-direction: row;
+  border-radius: 20px;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  padding-left: 10px;
+  padding-right: ${({ theme }) => theme.size.padding}px;
+  margin-left: ${({ theme }) => theme.size.padding}px;
+  margin-right: ${({ theme }) => theme.size.padding}px;
+  margin-top: ${({ theme }) => theme.size.padding}px;
+`;
+
+export const SearchInput = styled.TextInput.attrs({})`
+  padding-left: 10px;
+  border-radius: 10px;
+  height: 40px;
+  flex: 1;
+  font-family: 'RobotoRegular';
+`;
+
 export const StyledTitle = styled.Text`
-  margin-top: 20px;
   font-size: ${({ theme }) => theme.fontSize + 12}px;
   font-family: 'RobotoBlack';
   color: ${({ theme }) => theme.color.fill};
