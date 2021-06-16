df146db945c60641e1aa0d0ce279a6e56b2aa2d8
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-15 23:08:54 -0300

    :bug: Inserindo text para remover erro de prop.

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 // COMPONENTS
 import Container from '@components/Container';
+import { Text } from 'react-native';
 
-const Home = () => <Container></Container>;
+const Home = () => (
+  <Container>
+    <Text>Conteudis aqui!</Text>
+  </Container>
+);
 
 export default Home;
