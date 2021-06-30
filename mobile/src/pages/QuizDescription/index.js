4a8ae9069756a6e369d31fabfc39f5c47265120e
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-30 16:29:43 -0300

    :lipstick: Adicionando DIALOG ao clicar no botao de desistir de quiz em progreso.

--- mobile/src/pages/QuizDescription/index.js
+++ mobile/src/pages/QuizDescription/index.js
@@ -2,9 +2,13 @@
 import React, { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
 
 // API
 import api from '@api';
+
+// COMPONENTS
+import Dialog from '@components/Dialog';
 
 // HOOKS
 import useQuestions from '@hook/useQuestion';
@@ -40,6 +44,8 @@ const QuizDescription = ({ route }) => {
     route.params;
   const { title, description, tags, id, pin } = quiz;
 
+  const [visibleGiveUPModal, setVisibleGivUPModal] = useState(false);
+
   const [studentQuizID, setStudentQuizID] = useState(idStudentQuiz);
   const { getQuestionsOfQuizFromDatabase } = useQuestions();
   const navigation = useNavigation();
@@ -59,7 +65,7 @@ const QuizDescription = ({ route }) => {
   };
 
   const giveUPQuiz = async () => {
-    const { data } = await api.put('/studentQuiz/finishQuiz', {
+    await api.put('/studentQuiz/finishQuiz', {
       quiz_id: id,
       id_student_quiz: studentQuizID,
     });
@@ -68,67 +74,103 @@ const QuizDescription = ({ route }) => {
   };
 
   return (
-    <DetailsContainer fill="white">
-      <QuizDescriptionHeader>
-        <StyledImageBackground source={require('@assets/teste.jpg')}>
-          <GoBackButtonWrapper>
-            <StyledIconButton onPress={() => navigation.goBack()}>
-              <Ionicons name="chevron-back" size={32} color="white" />
-            </StyledIconButton>
-          </GoBackButtonWrapper>
-          {studentQuizID ? (
-            <ButtonWrapper resume>
-              <ButtonStyled onPress={giveUPQuiz}>
-                <GiveUPButtonWraper>
-                  <StyledText fill="white">DESISTIR</StyledText>
-                </GiveUPButtonWraper>
-              </ButtonStyled>
-              <ButtonStyled onPress={continueQuizAndGetAllQuestions}>
-                <ResumeButtonWrapper>
-                  <StyledIconButton>
-                    <Ionicons name="ios-play-circle" size={32} color="white" />
-                  </StyledIconButton>
-                  <StyledText fill="white">CONTINUAR</StyledText>
-                </ResumeButtonWrapper>
-              </ButtonStyled>
-            </ButtonWrapper>
-          ) : (
-            <ButtonWrapper resume={false}>
-              <ButtonStyled onPress={startQuizAndGetAllQuestions}>
-                <PlayButtonWrapper>
-                  <StyledIconButton>
-                    <Ionicons name="ios-play-circle" size={32} color="white" />
-                  </StyledIconButton>
-                  <StyledText fill="white">JOGAR</StyledText>
-                </PlayButtonWrapper>
-              </ButtonStyled>
-            </ButtonWrapper>
-          )}
-        </StyledImageBackground>
-      </QuizDescriptionHeader>
+    <>
+      <DetailsContainer fill="white">
+        <QuizDescriptionHeader>
+          <StyledImageBackground source={require('@assets/teste.jpg')}>
+            <GoBackButtonWrapper>
+              <StyledIconButton onPress={() => navigation.goBack()}>
+                <Ionicons name="chevron-back" size={32} color="white" />
+              </StyledIconButton>
+            </GoBackButtonWrapper>
+            {studentQuizID ? (
+              <ButtonWrapper resume>
+                <ButtonStyled onPress={() => setVisibleGivUPModal(true)}>
+                  <GiveUPButtonWraper>
+                    <StyledText fill="white">DESISTIR</StyledText>
+                  </GiveUPButtonWraper>
+                </ButtonStyled>
+                <ButtonStyled onPress={continueQuizAndGetAllQuestions}>
+                  <ResumeButtonWrapper>
+                    <StyledIconButton>
+                      <Ionicons
+                        name="ios-play-circle"
+                        size={32}
+                        color="white"
+                      />
+                    </StyledIconButton>
+                    <StyledText fill="white">CONTINUAR</StyledText>
+                  </ResumeButtonWrapper>
+                </ButtonStyled>
+              </ButtonWrapper>
+            ) : (
+              <ButtonWrapper resume={false}>
+                <ButtonStyled onPress={startQuizAndGetAllQuestions}>
+                  <PlayButtonWrapper>
+                    <StyledIconButton>
+                      <Ionicons
+                        name="ios-play-circle"
+                        size={32}
+                        color="white"
+                      />
+                    </StyledIconButton>
+                    <StyledText fill="white">JOGAR</StyledText>
+                  </PlayButtonWrapper>
+                </ButtonStyled>
+              </ButtonWrapper>
+            )}
+          </StyledImageBackground>
+        </QuizDescriptionHeader>
 
-      <StyledScrollView>
-        <BodyDescription>
-          <StyledTitle>PIN</StyledTitle>
-          <StyledPIN>{pin}</StyledPIN>
-          <StyledTitle>{title}</StyledTitle>
-          <StyledDescriptionText>{description}</StyledDescriptionText>
-          <StyledTitle>TAGS</StyledTitle>
-          <TagsContainer>
-            {tags.map((tag, index) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <StyledTag key={index}>{tag}</StyledTag>
-            ))}
-          </TagsContainer>
-        </BodyDescription>
-      </StyledScrollView>
-      {studentQuizID && (
-        <QuizProgress fill="purple">
-          <StyledTitleProgress>Questões repondidas: </StyledTitleProgress>
-          <StyledTextProgress>01/07</StyledTextProgress>
-        </QuizProgress>
-      )}
-    </DetailsContainer>
+        <StyledScrollView>
+          <BodyDescription>
+            <StyledTitle>PIN</StyledTitle>
+            <StyledPIN>{pin}</StyledPIN>
+            <StyledTitle>{title}</StyledTitle>
+            <StyledDescriptionText>{description}</StyledDescriptionText>
+            <StyledTitle>TAGS</StyledTitle>
+            <TagsContainer>
+              {tags.map((tag, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <StyledTag key={index}>{tag}</StyledTag>
+              ))}
+            </TagsContainer>
+          </BodyDescription>
+        </StyledScrollView>
+        {studentQuizID && (
+          <QuizProgress fill="purple">
+            <StyledTitleProgress>Questões repondidas: </StyledTitleProgress>
+            <StyledTextProgress>01/07</StyledTextProgress>
+          </QuizProgress>
+        )}
+      </DetailsContainer>
+      <Dialog
+        title="Deseja realmente desistir?"
+        visible={visibleGiveUPModal}
+        hideDialog={() => setVisibleGivUPModal(false)}
+        firstButtonOnPress={() => setVisibleGivUPModal(false)}
+        secondButtonOnPress={() => {
+          giveUPQuiz();
+          setVisibleGivUPModal(false);
+        }}
+        firstButtonLabel="CANCELAR"
+        secondButtonLabel="SIM"
+        lottieAnimation={
+          <LottieView
+            autoPlay
+            loop
+            style={{ width: 100 }}
+            resizeMode="cover"
+            speed={1}
+            // eslint-disable-next-line global-require
+            source={require('@assets/lottie/sad_emote.json')}
+          />
+        }
+      >
+        Seu score será calculado parcialmente de acordo com as questões já
+        respondidas!
+      </Dialog>
+    </>
   );
 };
 
