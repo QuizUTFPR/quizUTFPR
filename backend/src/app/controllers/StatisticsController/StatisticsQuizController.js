5d55f17e2fd72db4f189395dce4356c82999e492
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-07-02 10:36:00 -0300

    :bug: Arrumando parcialmente requisiçao de estatisticas do quiz.

--- backend/src/app/controllers/StatisticsController/StatisticsQuizController.js
+++ backend/src/app/controllers/StatisticsController/StatisticsQuizController.js
@@ -8,7 +8,8 @@ import Teacher from "../../models/TeacherModel";
 import Tag from "../../models/TagModel";
 import File from '../../models/FileModel';
 import Question from '../../models/QuestionModel'
-
+import Answer from '../../models/AnswerModel'
+import Student from '../../models/StudentModel'
 
 import StudentQuiz from "../../models/StudentQuiz";
 import StudentQuestionChoice from "../../models/StudentQuestionChoice";
@@ -29,77 +30,87 @@ class StatisticsQuizController {
         return res.status(404).json({
           error: "Quiz não encontrado!"
         });
+
       
-      const questions = await quiz.getQuestions({
-        attributes: ['id', 'index','title', 'id_image', 'timer', 'score', 'type', 'difficulty_level'],
-        joinTableAttributes: [],
-        order: [['index', 'ASC']]
-      });
-
-      // await Promise.all(questions.map(async item =>  console.log( await item.countQuestion_choice())));
-
-      if(!questions) 
-        return res.status(404).json({
-          error: "Quiz não possui questões cadastradas!"
-        });
+         const questions = await quiz.getQuestions({     
+           joinTableAttributes: [],
+           include: [{
+            model: Answer,
+            as: 'answer',
+            attributes: ['id', 'title', 'is_correct'],
+          }],
+          attributes: ['id', 'title', 'index', 'timer', 'score', 'difficulty_level', 'type'],
+          order: [['index', 'ASC']]
+         });
 
 
-
-      //GETTING THE HIGHEST SCORE FROM EACH STUDENT
-      let studentQuizAttempt = [];
-      (await quiz.getQuiz_student({
-        where: {
-          is_finished: true
-        },
-        attributes: ['id', 'student_id', 'quiz_id', 'hit_amount', 'score'],
-        order: [['student_id', 'ASC']]
-      })).map(item => {
-        const attempt = studentQuizAttempt[item.student_id];
-        if(!attempt){
-          studentQuizAttempt[item.student_id] = item;
-        }else{
-          if(attempt.score < item.score){
-            studentQuizAttempt[item.student_id] = item;
-          }
-        }
-      });
-
-      let SumTotalTimeAnswering = 0;
-
-      // GETTING THE CHOICES FROM EACH STUDENT
-      const studentQuizAttemptWithoutNull = studentQuizAttempt.filter(Boolean)
-      const formatedStudentQuizAttempt = await Promise.all((studentQuizAttemptWithoutNull.filter(Boolean))
-        .map(async (choice, index) => {
-          const student = await choice.getStudent({
-            attributes: ['name', 'email']
-          });
-          const student_choice = (await choice.getQuiz_question_choice({
-            attributes: ['question_id', 'time_left', 'checked1', 'checked2', 'checked3', 'checked4']
-          })).map(item => {
-            SumTotalTimeAnswering += questions[index].timer - item.time_left;
-            return item;
+        if(!questions) 
+          return res.status(404).json({
+            error: "Quiz não possui questões cadastradas!"
           });
 
-          return {
-            ...choice.dataValues,
-            student,
-            student_choice
-          }
-        }));
-    
-    
+
+        //  GETTING THE HIGHEST SCORE FROM EACH STUDENT
+
+          const studentQuizAttempt = await quiz.getQuiz_student({
+            where: {
+              is_finished: true
+            },
+            attributes: ['id', 'student_id', 'quiz_id'],
+            group: ["student_id"],
+          });
       
 
-      const returnedValue = {
-        avgTotalTimeAnswering: SumTotalTimeAnswering/formatedStudentQuizAttempt.length,
-        questions,
-        formatedStudentQuizAttempt
-      }
+        //  GETTING THE ATTEMPT FROM EACH STUDENT CONSIDERING THE HIGHEST SCORE
+         const formatedStudentQuizAttempt = await Promise.all(
+           studentQuizAttempt.map(async (choice, index) => {
+
+             const student = await choice.getStudent({
+               attributes: ['id', 'name', 'email'],
+               include: [
+                 {
+                   model: StudentQuiz,
+                   as: 'student_quiz',
+                   where: {
+                     quiz_id,
+                     is_finished: true
+                   },
+                   attributes: ['id', 'hit_amount', 'score']
+                  }
+                ],
+                order: [[
+                  {
+                    model: StudentQuiz,
+                    as: 'student_quiz',
+                  }, 'score', 'DESC'
+                ]],
+             });
+
+             return {
+              ...student.dataValues, 
+              student_quiz: {
+                ...student.student_quiz[0].dataValues,
+                choices: await student.student_quiz[0].getQuiz_question_choice({
+                  attributes: ['id', 'student_quiz_id','question_id', 'time_left', 'checked1', 'checked2', 'checked3', 'checked4']
+
+                })
+              }
+              }
+           }));
+    
+        
+        // const student_choices = formatedStudentQuizAttempt.map(async item => await item.student_quiz.getQuiz_question_choice());
+
+       const returnedValue = {
+        //  avgTotalTimeAnswering: SumTotalTimeAnswering/formatedStudentQuizAttempt.length,
+         questions,
+         formatedStudentQuizAttempt
+       }
 
       
 
 
-    return res.status(200).json({returnedValue });
+    return res.status(200).json(returnedValue);
 
     }catch(err){
       console.log(err)
