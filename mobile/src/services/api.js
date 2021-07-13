423d31a8a4d09a4a8e637488e255ee0c34428fb1
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-07-13 09:48:27 -0300

    :whales: Atualizando volumes docker.

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // const ip = '10.0.2.2';
-const ip = '192.168.15.9';
+const ip = '192.168.15.5';
 
 const api = axios.create({
   baseURL: `http://${ip}:3333`,
