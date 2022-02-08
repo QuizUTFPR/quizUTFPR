1ab3d4889bf2a91a858e399e31239bb72da6e1f7
JessePires <jesserocha@alunos.utfpr.edu.br>
2022-02-08 18:19:21 -0300

    :recycle: Criando Repository de Answer.

---
+++ backend/src/app/repositories/Question.js
@@ -0,0 +1,13 @@
+// import Question from '../models/QuestionModel';
+
+// class QuestionRepository {
+//   async create(question) {
+//     return Question.create(question);
+//   }
+
+//   async findById(id) {
+//     return Question.findByPk(id);
+//   }
+// }
+
+// export default QuestionRepository;
