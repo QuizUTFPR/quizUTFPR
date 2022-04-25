1981160fb8aa9e8fcd5178cc5a7a7da742bf0bd8
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2022-04-25 11:16:17 -0300

    :sparkles: Arrumando login teacher.

--- backend/src/app/services/Teacher/CreateTeacherSession.js
+++ backend/src/app/services/Teacher/CreateTeacherSession.js
@@ -31,32 +31,32 @@ class CreateTeacherSessionService {
     const { username, password } = data;
 
     // OBTENDO TOKEN PARA CONSEGUIR UTILIZAR API DO LDAP
-    // const responseLDAP = await axios.post(`${process.env.LDAP_URL}/login`, {
-    //   username: process.env.LDAP_USERNAME,
-    //   password: process.env.LDAP_PASSWORD,
-    // });
+    const responseLDAP = await axios.post(`${process.env.LDAP_URL}/login`, {
+      username: process.env.LDAP_USERNAME,
+      password: process.env.LDAP_PASSWORD,
+    });
 
-    // const ldapToken = responseLDAP.data.token;
+    const ldapToken = responseLDAP.data.token;
 
     // VERIFICANDO SE OS DADOS ESTÃO CORRETOS DE ACORDO COM O LDAP
-    // const responseLoginLDAP = await axios.post(
-    //   `${process.env.LDAP_URL}/ldap/doLogin`,
-    //   {
-    //     username,
-    //     password,
-    //   },
-    //   {
-    //     headers: {
-    //       Authorization: `Bearer ${ldapToken}`,
-    //     },
-    //   }
-    // );
+    const responseLoginLDAP = await axios.post(
+      `${process.env.LDAP_URL}/ldap/doLogin`,
+      {
+        username,
+        password,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${ldapToken}`,
+        },
+      }
+    );
 
-    // const {
-    //   email: teacherEmail,
-    //   name: teacherName,
-    //   // dn: personCategory,
-    // } = responseLoginLDAP.data;
+    const {
+      email: teacherEmail,
+      name: teacherName,
+      // dn: personCategory,
+    } = responseLoginLDAP.data;
 
     // IMPEDIR ALUNOS DE SE CONECTAR NO 1 DE CONTROLE
     // if ((personCategory.indexOf('alunos') !== -1)){
@@ -71,30 +71,30 @@ class CreateTeacherSessionService {
     });
 
     // FORMATANDO NOME DO PROFESSOR
-    // const splittedName = teacherName.split(' ');
-    // const formatedName = `${splittedName[0]} ${
-    //   splittedName[splittedName.length - 1]
-    // }`;
+    const splittedName = teacherName.split(' ');
+    const formatedName = `${splittedName[0]} ${
+      splittedName[splittedName.length - 1]
+    }`;
 
     // CASO NÃO EXISTA CRIO UMA CONTA NO BANCO PARA O MESMO
     // USADO NOS TESTES DENTRO DA REDE DA UTF
-    // if (!teacher)
-    //   teacher = await this.teacherRepository.create({
-    //     uid: username,
-    //     name: 'formatedName',
-    //     email: 'teacherEmail@gmail.com',
-    //   });
+    if (!teacher)
+      teacher = await this.teacherRepository.create({
+        uid: username,
+        name: 'formatedName',
+        email: 'teacherEmail@gmail.com',
+      });
 
-    // if (!teacher)
-    //   teacher = await this.teacherRepository.create({
-    //     uid: username,
-    //     name: formatedName,
-    //     email: teacherEmail,
-    //   });
+    if (!teacher)
+      teacher = await this.teacherRepository.create({
+        uid: username,
+        name: formatedName,
+        email: teacherEmail,
+      });
 
-    // if (!(await teacher.checkPassword(password))) {
-    //   return res.status(403).json({ error: 'Senha Incorreta!' });
-    // }
+    if (!(await teacher.checkPassword(password))) {
+      return res.status(403).json({ error: 'Senha Incorreta!' });
+    }
 
     const { id, name, uid } = teacher;
 
@@ -109,7 +109,7 @@ class CreateTeacherSessionService {
     return {
       teacher: {
         name,
-        email: 'teacherEmail',
+        email: teacherEmail,
         uid,
       },
       token,
