47c42a03dd66e5a0392d0ff760a9e6303b87d34f
Iago Carmona <iagoortegacarmona@gmail.com>
2022-10-25 00:21:20 -0300

    :wrench: Fazendo correções no projeto.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,6 +4,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   define: {
