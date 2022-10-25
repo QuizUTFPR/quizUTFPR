7b9219afe233f39ec093ad65ece012a71966674a
Iago Carmona <iagoortegacarmona@gmail.com>
2022-10-25 00:17:59 -0300

    :wrench: Fazendo correções no projeto.

--- mobile/env.js
+++ mobile/env.js
@@ -1,3 +1,8 @@
 export const API_URL = 'http://10.0.2.2:3333'; // JESSÉ
 // export const API_URL = 'http://quizapi.dacom.cm.utfpr.edu.br'; // JHONATAN
 // export const API_URL = 'http://192.168.1.9:3333'; // JHONATAN
+
+// Google OAuth2 config EXPO
+export const EXPO_CLIENT_ID =
+  '886529009031-0lip6dor80hc90fncbg97s76ipbgu2vu.apps.googleusercontent.com';
+export const EXPO_CLIENT_SECRET = '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';
