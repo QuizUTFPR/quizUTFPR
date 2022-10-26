7771a02b79f9bde4c14791d9f308c7a78bc32ce1
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-10-26 15:30:56 -0300

    .env api set

--- mobile/env.js
+++ mobile/env.js
@@ -1,8 +1,9 @@
-export const API_URL = 'http://10.0.2.2:3333'; // JESSÉ
+// export const API_URL = 'http://10.0.2.2:3333'; // JESSÉ
 // export const API_URL = 'http://quizapi.dacom.cm.utfpr.edu.br'; // JHONATAN
 // export const API_URL = 'http://192.168.1.9:3333'; // JHONATAN
+export const API_URL = 'http://192.168.237.65:3333'; //VITOR
 
 // Google OAuth2 config EXPO
 export const EXPO_CLIENT_ID =
-  '886529009031-0lip6dor80hc90fncbg97s76ipbgu2vu.apps.googleusercontent.com';
+    '886529009031-0lip6dor80hc90fncbg97s76ipbgu2vu.apps.googleusercontent.com';
 export const EXPO_CLIENT_SECRET = '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';
