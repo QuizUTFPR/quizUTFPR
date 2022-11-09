0309e58a705dba8c0dc088ad8e0d07df1c7dcc7d
Iago Carmona <iagoortegacarmona@gmail.com>
2022-11-09 13:45:33 -0300

    :rocket: merging

--- mobile/env.js
+++ mobile/env.js
@@ -4,6 +4,6 @@
 export const API_URL = 'http://192.168.237.65:3333'; // VITOR
 
 // Google OAuth2 config EXPO
-export const EXPO_CLIENT_ID =
+export const expoClientId =
   '886529009031-0lip6dor80hc90fncbg97s76ipbgu2vu.apps.googleusercontent.com';
-export const EXPO_CLIENT_SECRET = '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';
+export const expoClientSecret = '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';
