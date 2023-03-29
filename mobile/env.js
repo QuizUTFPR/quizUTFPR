cde0902347aa0250258e5698f2b2b9c223dc58a3
Iago Carmona <iagoortegacarmona@gmail.com>
2023-03-29 14:11:48 -0300

    env mobile

--- mobile/env.js
+++ mobile/env.js
@@ -1,6 +1,9 @@
 // export const API_URL = 'http://10.0.2.2:3333'; // JESSÉ
 // export const API_URL = 'http://quizapi.dacom.cm.utfpr.edu.br'; // JHONATAN
-export const API_URL = 'http://192.168.1.9:3333'; // IAGO
+// export const API_URL = 'http://192.168.1.9:3333'; // IAGO CM
+// export const API_URL = 'http://172.17.0.1:3333'; // IAGO Cianorte
+export const API_URL = 'http://localhost:3333';
+
 // export const API_URL = 'http://10.0.2.2:3333'; // VITOR
 
 // Google OAuth2 config EXPO
