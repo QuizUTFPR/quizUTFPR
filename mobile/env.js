c57bb74020448fcbb20dd41bcdee758c5e00393a
Iago Carmona <iagoortegacarmona@gmail.com>
2023-06-12 23:30:38 -0300

    Removendo comentarios

--- mobile/env.js
+++ mobile/env.js
@@ -1,15 +1,6 @@
-// export const API_URL = 'http://10.0.2.2:3333'; // JESSÉ
-// export const API_URL = 'http://quizapi.dacom.cm.utfpr.edu.br'; // JHONATAN
-// export const API_URL = 'http://192.168.1.9:3333'; // IAGO CM
-// export const API_URL = 'http://172.17.0.1:3333'; // IAGO Cianorte
-// export const API_URL = 'http://192.168.100.5:3333';
-
- // IAGO CM
-export const API_URL = 'http://192.168.1.9:3333';
-// export const API_URL = 'http://172.17.0.1:3333'; // IAGO Cianorte
-// export const API_URL = 'http://localhost:3333';
+export const API_URL = 'https://quizapi.dacom.cm.utfpr.edu.br';
 
 // Google OAuth2 config EXPO
 export const expoClientId =
-  '886529009031-0lip6dor80hc90fncbg97s76ipbgu2vu.apps.googleusercontent.com';
+  '886529009031-6hi089mr7iccg1dtrh3m3s26qkmehq77.apps.googleusercontent.com';
 export const expoClientSecret = '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';
