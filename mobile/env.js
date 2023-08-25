8891e06f9c81f07de2b64be4aefb18d9ab5ecaaa
Iago Carmona <iagoortegacarmona@gmail.com>
2023-08-25 10:55:13 -0300

    Atualizando env.js mobile

--- mobile/env.js
+++ mobile/env.js
@@ -1,6 +1,13 @@
 export const API_URL = 'https://quizapi.dacom.cm.utfpr.edu.br';
 
-// Google OAuth2 config EXPO
-export const expoClientId =
-  '886529009031-6hi089mr7iccg1dtrh3m3s26qkmehq77.apps.googleusercontent.com';
-export const expoClientSecret = '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';
+// Google OAuth2 config EXPO // PRODUCTION
+// export const expoClientId = '886529009031-6hi089mr7iccg1dtrh3m3s26qkmehq77.apps.googleusercontent.com';
+
+// Google OAuth2 config EXPO // DEVELOPMENT
+export const expoClientId = '886529009031-0lip6dor80hc90fncbg97s76ipbgu2vu.apps.googleusercontent.com';
+
+// Google API to fetch user info
+export const googleApi = 'https://www.googleapis.com/userinfo/v2/me';
+
+// ambient: development | production
+export const ambient = 'development';
