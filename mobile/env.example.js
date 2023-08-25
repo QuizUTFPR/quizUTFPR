9f19d39058efcd9f0bd57a78af0247d990d4a25c
Iago Carmona <iagoortegacarmona@gmail.com>
2023-08-25 10:54:59 -0300

    Atualizando env.example

--- mobile/env.example.js
+++ mobile/env.example.js
@@ -2,4 +2,9 @@ export const API_URL = 'http://10.0.2.2:3333';
 
 // Google OAuth2 config EXPO
 export const expoClientId = 'google-expo-client-id';
-export const expoClientSecret = 'google-expo-client-secret';
+
+// Google API to fetch user info
+export const googleApi = 'https://www.googleapis.com/userinfo/v2/me';
+
+// ambient: development | production
+export const ambient = 'development';
